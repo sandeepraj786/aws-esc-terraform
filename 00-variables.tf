d177d0c7ea4a179e6e23108db6e45bf64cb870e9
Sandeep Raj <sandeep.raj@nium.com>
2021-11-20 21:45:56 +0530

    added

--- 00-variables.tf
+++ 00-variables.tf
@@ -4,12 +4,12 @@ variable "environment" {}
 
 # variable "default_region" {}
 
-variable "aws_access_key" {
+variable "access_key" {
   type = string
   description = "The AWS access key."
 }
 
-variable "aws_secret_key" {
+variable "secret_key" {
   description = "The AWS secret key."
   type = string
 }
